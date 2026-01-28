7d06f00773eaecb1cdacc5b8a9d4f5d0a3ed15f2
Dr. Patrick Urbanke <patrick@getml.com>
2026-01-28 22:47:16 +0100

    Use ruff instead of black

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,7 +1,7 @@
 sudo apt-get update -y
 sudo apt-get install -y gcc g++ make cmake git clang-format build-essential cmake 
 sudo apt-get install -y python3-dev python3-pip 
-sudo pip3 install pylint mypy black pynvim --break-system-packages
+sudo pip3 install pylint mypy ruff pynvim --break-system-packages
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 
