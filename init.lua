d459e2c3909407f002611c5f3f8c481f723856c1
Dr. Patrick Urbanke <patrick@getml.com>
2026-05-25 11:51:32 +0200

    Enable Go code completion, remove trailing whitespaces on save

--- init.lua
+++ init.lua
@@ -6,6 +6,14 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.wo.number = true
 
+-- remove trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.cmd([[%s/\s\+$//e]])
+  end,
+})
+
 -- Configure LSP clients
 
 -- Set default root markers for all clients
@@ -13,11 +21,12 @@ vim.lsp.config('*', {
   root_markers = { '.git' },
 })
 
-vim.lsp.enable('python_llm_code_completion')
 vim.lsp.enable('python_ruff')
 vim.lsp.enable('python_pyrefly')
 vim.lsp.enable('python_ty')
 
+vim.lsp.enable('go_llm_code_completion')
+vim.lsp.enable('python_llm_code_completion')
 vim.lsp.enable('rust_llm_code_completion')
 
 -- set autocomplete behavior.
@@ -37,6 +46,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.lsp.completion.enable(true, clientID, 0, { autotrigger = true })
     end,
 })
+
+
+-- open hover info when pressing <F2> in normal mode
+vim.keymap.set('n', '<F2>', function()
+    vim.lsp.buf.hover()
+end)
 
 -- open autocomplete menu when pressing <F2> in insert mode
 vim.keymap.set('i', '<F2>', function()
---
+++ lsp/go_llm_code_completion.lua
@@ -0,0 +1,5 @@
+return {
+  cmd = {'llm-code-completion', '--programming-language=go'},
+  filetypes = { "go" },
+  root_markers = { ".git" },
+}
