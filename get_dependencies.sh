2288faaf910004d65e3dd8cb52a5bc67aa54ee8f
Dr. Patrick Urbanke <urbanke.patrick@gmail.com>
2026-05-27 22:43:31 +0200

    Added rust to the dependencies

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -3,5 +3,7 @@ sudo apt-get install -y gcc g++ make cmake git clang-format clangd build-essenti
 sudo apt-get install -y python3-dev python3-pip
 sudo pip3 install pylint mypy ruff pynvim --break-system-packages
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 
